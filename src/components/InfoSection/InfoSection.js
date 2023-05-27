2008f0c7854b45c6fceebf609e614f743266ca54
Yasin A. Yokus <ayokus@icloud.com>
2023-05-27 23:25:13 +0300

    add website with content

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {InfoSec, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle, ImgWrapper, Img} from './InfoSection.elements'
+import { Container, Button } from '../../globalStyles'
+import { Link } from 'react-router-dom'
+
+ const InfoSection = ({ 
+    
+    primary,
+    lightBg,
+    topLine,
+    lightTopLine,
+    lightText,
+    lightTextDesc,
+    headline,
+    description,
+    buttonLabel,
+    img,
+    alt,
+    imgStart,
+    start
+}) => {
+    return (
+        <>
+            <InfoSec lightBg={lightBg}>
+                <Container>
+                    <InfoRow imgStart={imgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
+                            <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                            <Heading lightText={lightText}>{headline}</Heading>
+                            <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                            
+                            <a href='mailto:mhyokus35@outlook.com'>
+                            <Button big fontBig primary={primary}>
+                                {buttonLabel}
+                            </Button>
+                            </a>
+                            </TextWrapper>
+                        </InfoColumn>
+                        <InfoColumn>
+                        <ImgWrapper start={start}>
+                            <Img src={img} alt={alt} />
+                        </ImgWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
+            </InfoSec>
+        </>
+    )
+}
+
+export default InfoSection;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,72 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Gayrimenkul Hizmetinde Tek Adres',
+    headline: 'Yılların verdiği tecrübe ile sizlere hizmet vermekten gurur duyuyoruz.',
+    description:
+      // 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+      'Aklınızdaki gayrimenkulü bulmak için bize ulaşın.',
+    buttonLabel: 'Bize Ulaşın',
+    imgStart: '',
+    // img: require('../../images/svg-1.svg'),
+    img: require('../../images/house.jpg'),
+    alt: 'Credit Card',
+    start: ''
+  };
+  
+  // export const homeObjTwo = {
+  //   primary: true,
+  //   lightBg: false,
+  //   lightTopLine: true,
+  //   lightText: true,
+  //   lightTextDesc: true,
+  //   topLine: 'Instant Setup',
+  //   headline: 'Extremely quick onboarding process',
+  //   description:
+  //     "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+  //   buttonLabel: 'Learn More',
+  //   imgStart: '',
+  //   // img: require('../../images/svg-2.svg'),
+  //   img: require('../../images/real-estate.jpg'),
+  //   alt: 'Vault',
+  //   start: ''
+  // };
+  
+  export const homeObjThree = {
+    primary: false,
+    lightBg: true,
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Müşterilerimizden',
+    headline:
+      'Farklı bir bakış açısı ile gayrimenkul hizmeti almak isteyenler için doğru adres.',
+    description:
+      "İhtiyaçlarımızı en iyi şekilde anlayıp, bize en uygun gayrimenkulü buldular. Teşekkürler.",
+    buttonLabel: 'Referanlarımız',
+    imgStart: 'start',
+    // img: require('../../images/profile.jpg'),
+    img: require('../../images/real-estate.jpg'),
+    alt: 'Vault',
+    start: 'true'
+  };
+  
+  // export const homeObjFour = {
+  //   primary: true,
+  //   lightBg: false,
+  //   lightTopLine: true,
+  //   lightText: true,
+  //   lightTextDesc: true,
+  //   topLine: 'Secure Database',
+  //   headline: 'All your data is stored on our secure server',
+  //   description:
+  //     'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  //   buttonLabel: 'Sign Up Now',
+  //   imgStart: 'start',
+  //   img: require('../../images/svg-3.svg'),
+  //   alt: 'Vault',
+  //   start: 'true'
+  // };
